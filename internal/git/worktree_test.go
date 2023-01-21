5510e3d11145200a762a8f924e147ddab7a48ff2
michaelhass <4216942+michaelhass@users.noreply.github.com>
2023-01-21 22:22:26 +0900

    modify tests

--- internal/git/worktree_test.go
+++ internal/git/worktree_test.go
@@ -42,12 +42,26 @@ func TestStatus(t *testing.T) {
 
 	if status.contains(status.Staged, unmodifiedPath, Unmodified) ||
 		status.contains(status.Unstaged, unmodifiedPath, Unmodified) {
-		t.Errorf("Failed to remove unmodified status")
+		t.Errorf("Failed to remove unmodified status.")
 	}
 
 	if status.contains(status.Staged, untrackedPath, Untracked) ||
 		!status.contains(status.Unstaged, untrackedPath, Untracked) {
-		t.Errorf("Failed to remove unmodified status")
+		t.Errorf("Failed to remove unmodified status.")
 	}
 
+	if !status.contains(status.Staged, onlyStagedPath, Modified) ||
+		status.contains(status.Unstaged, onlyStagedPath, Modified) {
+		t.Errorf("File should be included in staged but not in unstaged.")
+	}
+
+	if status.contains(status.Staged, onlyUnstagedPath, Modified) ||
+		!status.contains(status.Unstaged, onlyUnstagedPath, Modified) {
+		t.Errorf("File should be included in unstaged but not in staged.")
+	}
+
+	if !status.contains(status.Staged, bothPath, Modified) ||
+		!status.contains(status.Unstaged, bothPath, Modified) {
+		t.Errorf("File should be included in both staged and unstaged.")
+	}
 }
