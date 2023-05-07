47bcb51934c1de5d8a2c646266c28315c978582b
michaelhass <4216942+michaelhass@users.noreply.github.com>
2023-05-07 16:48:25 +0900

    Change key matching

--- internal/ui/app/app.go
+++ internal/ui/app/app.go
@@ -31,8 +31,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch msg.Type {
+		case tea.KeyCtrlC:
 			return m, tea.Quit
 		}
 	case tea.WindowSizeMsg:
