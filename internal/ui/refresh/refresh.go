43d03862b33939a42545dbf366be6f6230437ac2
michaelhass <git@mhass.me>
2025-05-14 11:11:57 +0200

    Add docs

--- internal/ui/refresh/refresh.go
+++ internal/ui/refresh/refresh.go
@@ -6,8 +6,10 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+// Msg produced by Schedule command
 type Msg time.Time
 
+// Schedule produces a Msg after the given duration.
 func Schedule(d time.Duration) tea.Cmd {
 	return tea.Tick(d, func(t time.Time) tea.Msg {
 		return Msg(t)
