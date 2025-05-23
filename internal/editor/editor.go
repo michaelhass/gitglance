190041786abdddac6a1170763828fb105f0b6e75
michaelhass <git@mhass.me>
2025-05-23 09:12:32 +0200

    Get global git core.editor

--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -7,36 +7,36 @@ import (
 	"strings"
 )
 
-type cmd struct {
+type Cmd struct {
 	name string
 	arg  []string
 }
 
-type CreateEditorCmdOption func() (*cmd, error)
+type CreateEditorCmdOption func() (*Cmd, error)
 
-func CreateFallbackEditor() *cmd {
-	return &cmd{name: "vi", arg: []string{}}
+func newFallbackEditorCmd() *Cmd {
+	return &Cmd{name: "vi", arg: []string{}}
 }
 
-func EnvVisual() (*cmd, error) {
+func EnvVisual() (*Cmd, error) {
 	return newCmdFromString(os.Getenv("VISUAL"))
 }
 
-func EnvEditor() (*cmd, error) {
+func EnvEditor() (*Cmd, error) {
 	return newCmdFromString(os.Getenv("EDITOR"))
 }
 
 func WithCmdString(fn func() (string, error)) CreateEditorCmdOption {
-	return func() (*cmd, error) {
+	return func() (*Cmd, error) {
 		value, err := fn()
 		if err != nil {
-			value = " "
+			value = ""
 		}
 		return newCmdFromString(value)
 	}
 }
 
-func newCmdFromString(cmdString string) (*cmd, error) {
+func newCmdFromString(cmdString string) (*Cmd, error) {
 	cmdString = strings.TrimSpace(cmdString)
 	components := []string{}
 	for _, component := range strings.Split(cmdString, " ") {
@@ -48,9 +48,14 @@ func newCmdFromString(cmdString string) (*cmd, error) {
 		return nil, errors.New("Empty")
 	}
 
-	return &cmd{name: components[0], arg: components[1:]}, nil
+	return &Cmd{name: components[0], arg: components[1:]}, nil
 }
 
+// OpenFileCmdDefault produces an exec.Cmd to open a path in an editor.
+// It tries to prioritize the given list of editors before accessing a list of default editors.
+// default editor options with priority:
+// 1. env VISUAL
+// 2. env EDITOR
 func OpenFileCmdDefault(path string, prioritize ...CreateEditorCmdOption) *exec.Cmd {
 	defaultCreateEditorOpts := []CreateEditorCmdOption{
 		EnvVisual,
@@ -60,8 +65,11 @@ func OpenFileCmdDefault(path string, prioritize ...CreateEditorCmdOption) *exec.
 	return OpenFileCmd(path, all...)
 }
 
+// OpenFileCmd produces an exec.Cmd to open a path in an editor.
+// It will use the first successful CreateEditorCmdOption.
+// Otherwise it will fallback to "vi".
 func OpenFileCmd(path string, opts ...CreateEditorCmdOption) *exec.Cmd {
-	editorCmd := CreateFallbackEditor()
+	editorCmd := newFallbackEditorCmd()
 	for _, opt := range opts {
 		if other, err := opt(); err == nil {
 			editorCmd = other
