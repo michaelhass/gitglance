82d1211a817896462e07ab5b6e60855f29eed4db
michaelhass <git@mhass.me>
2025-05-23 10:41:33 +0200

    test failed build

--- internal/textwrap/builder_test.go
+++ internal/textwrap/builder_test.go
@@ -5,21 +5,23 @@ import (
 )
 
 func TestBuilderWriteMultineText(t *testing.T) {
-	var (
-		text    = "01234567\n12 34 567\nend"
-		builder = NewBuilder()
+	t.Error("Just a test error for github actions")
+	// return
+	// var (
+	// 	text    = "01234567\n12 34 567\nend"
+	// 	builder = NewBuilder()
 
-		expect = "01234\n567\n12 34\n 567\nend"
-		got    string
-	)
+	// 	expect = "01234\n567\n12 34\n 567\nend"
+	// 	got    string
+	// )
 
-	builder.SetLineLength(5)
-	builder.WriteString(text)
-	got = builder.String()
+	// builder.SetLineLength(5)
+	// builder.WriteString(text)
+	// got = builder.String()
 
-	if expect != got {
-		t.Errorf("[%s] is not equal to [%s]", expect, got)
-	}
+	// if expect != got {
+	// 	t.Errorf("[%s] is not equal to [%s]", expect, got)
+	// }
 }
 
 func TestBuilderWriteMultiLineWithTab(t *testing.T) {
