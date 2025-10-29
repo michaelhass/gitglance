6ab492767db3efb2119293dce364ea8651b29225
michaelhass <git@mhass.me>
2025-10-29 10:59:29 +0100

    add default err message

--- internal/core/err/err.go
+++ internal/core/err/err.go
@@ -5,3 +5,27 @@ type Msg interface {
 	ErrorTitle() string
 	ErrorDescription() string
 }
+
+type errMsg struct {
+	title string
+	err   error
+}
+
+func (e errMsg) Err() error {
+	return e.err
+}
+
+func (e errMsg) ErrorTitle() string {
+	return e.title
+}
+
+func (e errMsg) ErrorDescription() string {
+	if e.err == nil {
+		return ""
+	}
+	return e.err.Error()
+}
+
+func NewMsg(title string, err error) Msg {
+	return errMsg{title: title, err: err}
+}
